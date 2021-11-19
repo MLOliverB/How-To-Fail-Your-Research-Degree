f37c5c763e138ad1b5d3fe464efd346cf3e0b6dd
AnimatorOfSouls <fantailfishy@gmail.com>
2021-11-19 20:03:07 +0000

    Issue #70 - Put activities card data into database
    - Changed the id in the Big Cards table in `TP Database.xlsx` to begin 
    at 1
    - Converted the Big Cards table in the `TP Database.xlsx` file to 
    `Events.csv`
    - Changed id fields in both tables to **not** be autoincrementing, since 
    the number of entries will never change, and it will ensure that 
    referencing the IDs will always be consistent if for whatever reason the 
    order the cards are added changes.
    - Deleted the number field in the Events table and in Big Cards table in 
    `TP Database.xlsx` because all the cards only appear once
    - Added code to insert data from `Events.csv` into `Cards.db` in 
    `card_database_setup.py`
    - Updated README.md in the `data` folder to show the new fields in 
    Cards.db
    
    Closed [Issue 
    #70](https://stgit.dcs.gla.ac.uk/team-project-h/2021/cs01/cs01-main/-/issues/70)

--- data/README.md
+++ data/README.md
@@ -2,7 +2,7 @@
 
 This folder contains the data for the game
 - `Cards.db` contains the activity and event cards
-- `TP Database` contains the data about the activity and event cards to be read into the database
+- `TP Database.xlsx` contains the data about the activity and event cards to be read into the database
 
 
 ### Cards.db - Tables and Fields
@@ -26,9 +26,9 @@ This folder contains the data for the game
 |-|-|-|
 | id | Integer | Unique reference number |
 | stage | Integer | The stage of the game this card is played in. 1=Plan, 2=Context, 3=Implementation, 4=Write Up |
-| number | Integer | The number of times this card appears in the deck |
 | image | Text | The name of the image file (`something.png`) |
 | title | Text | The text displayed on the card |
 | description | Text | A description of what the card means |
+| save_condition | Text | The conditions to be allowed to ignore the effects of a card with a bad effect |
 | requirement | Text | The requirements for the card to take effect. `14:2&!2` means that card 14 must appear twice, and card 2 cannot appear |
 | effect | Text | The effect of the card. `n-1:3:2` means that three of the cards in stage 2 must be removed (`n/p/s/o/i` = remove a card, add a card, stand in for a card, block out spaces, ignore effects of another event card; `a:b:c` a=id of a specific card (0=not a specific card), b=number of cards to change, c=stage of card if not a specific card) |
--- setup/card_database_setup.py
+++ setup/card_database_setup.py
@@ -5,56 +5,24 @@ conn = sqlite3.connect("../data/Cards.db")
 c =  conn.cursor()
 
 ### Creating Tables
-conn.execute("CREATE TABLE IF NOT EXISTS Activities(id INTEGER PRIMARY KEY AUTOINCREMENT, stage INT, number INT, image TEXT, title TEXT, description TEXT, placement TEXT)")
-conn.execute("CREATE TABLE IF NOT EXISTS Events(id INTEGER PRIMARY KEY AUTOINCREMENT, stage INT, number INT, title TEXT, description TEXT, requirement TEXT, effect TEXT)")
+conn.execute("CREATE TABLE IF NOT EXISTS Activities(id INT PRIMARY KEY, stage INT, number INT, image TEXT, title TEXT, description TEXT, placement TEXT)")
+conn.execute("CREATE TABLE IF NOT EXISTS Events(id INT PRIMARY KEY, stage INT, image TEXT, title TEXT, description TEXT, save_condition TEXT, requirement TEXT, effect TEXT)")
 
  
 
 ### Inserting Data
 add_activity = "INSERT INTO Activities(id,stage,number,image,title,description,placement) VALUES (?,?,?,?,?,?,?)"
-#add_event = "INSERT INTO Events(id,stage,number,image,title,description,requirement,effect) VALUES (?,?,?,?,?,?,?,?)"
+add_event = "INSERT INTO Events(id,stage,image,title,description,save_condition,requirement,effect) VALUES (?,?,?,?,?,?,?,?)"
 
 with open("../data/Activities.csv") as file:
 	reader = csv.reader(file)
 	for row in reader:
 		c.execute(add_activity,row)
 
-
-"""
-#Connecting to database and creating a cursor to navigate the database
-conn = sqlite3.connect("data/MouseDatabase.db")
-cursor = conn.cursor()
-
-sql_embed = "INSERT INTO Embeds(name, emoji, colour) VALUES (?,?,?)"
-sql_theme = "INSERT INTO Themes(name, emoji, coinEmbed, coinTheme) VALUES (?,?,?,?)"
-
-#making lists of data to be added
-values_embeds = []
-values_embeds.append(["Red","<:red:837765748670857287>","#FF0600"])
-values_embeds.append(["Orange","<:orange:837765758406230077>","#FF8000"])
-values_embeds.append(["Yellow","<:yellow:837765888802947092>","#FFED00"])
-values_embeds.append(["Green","<:green:837765897003073636>","#00FF1A"])
-values_embeds.append(["Blue","<:blue:837765904845635646>","#00EFFF"])
-values_embeds.append(["Purple","<:purple:837765914052263956>","#4D00FF"])
-values_embeds.append(["Pink","<:pink:837765922013708298>","#FF00F9"])
-
-values_themes = []
-values_themes.append(["Mouse",":mouse2:",":cheese:",":peanuts:"])
-values_themes.append(["Frog",":frog:",":fly:",":cricket:"])
-values_themes.append(["Spider",":spider:",":mosquito:",":butterfly:"])
-values_themes.append(["Duck",":duck:",":bread:",":snail:"])
-
-#inserting data
-for v in values_embeds:
-	cursor.execute(sql_embed,v)
-for v in values_themes:
-	cursor.execute(sql_theme,v)
-
-conn.commit()
-cursor.close()
-conn.close()
-"""
-
+with open("../data/Events.csv") as file:
+	reader = csv.reader(file)
+	for row in reader:
+		c.execute(add_event,row)
 
 conn.commit()
 c.close()
