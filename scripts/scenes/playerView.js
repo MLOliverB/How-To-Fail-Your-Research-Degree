ab3f48e2c2deee685dabaefa9bdefbf8c1d24100
Solaris Viesseri <2520708l@student.gla.ac.uk>
2022-01-13 11:59:47 +0000

    #?? - Added a toolbar button class
    ## Changes Made
    - Added a class to make buttons on the toolbar
    - Moved the position of the discard card button

--- scripts/scenes/playerView.js
+++ scripts/scenes/playerView.js
@@ -1,4 +1,4 @@
-import { CardBox, AddCardBox, CardDiscardBox, goToNextStage, pickUpCard, activityImageName, eventImageName } from "../activity_cards/GameBoard.js";
+import { CardBox, AddCardBox, CardDiscardBox, ToolbarButton, goToNextStage, pickUpCard, activityImageName, eventImageName } from "../activity_cards/GameBoard.js";
 import { loadActivityCardStack, shuffleCardStack } from "../cards-management.js";
 
 /**
@@ -27,11 +27,12 @@ export default class playerView extends Phaser.Scene {
         this.width = this.cameras.main.displayWidth;
         this.height = this.cameras.main.displayHeight;
         
-        // rendering the game board
-        this.add.rectangle(this.x, this.y, this.width, this.height, 0xede0d4);    // background
-        this.add.rectangle(this.x, this.y*0.77, this.width, this.height, 0xf4a261).setScale(0.98, 0.75);  // playing board
-        this.add.rectangle(this.x, this.y*1.95, this.width, this.height, 0x023047).setScale(1, 0.2); // toolbar
-        this.cardBox = this.add.rectangle(this.x, this.y*1.76, this.width, this.height, 0xe76f51).setScale(0.162, 0.204); // card the player is holding
+		
+        // setting up the game board
+        this.add.rectangle(this.x, this.y, this.width, this.height, 0xede0d4);    											// background
+        this.add.rectangle(this.x, this.y*0.77, this.width, this.height, 0xf4a261).setScale(0.98, 0.75);					// playing board
+        this.add.rectangle(this.x, this.y*1.95, this.width, this.height, 0x023047).setScale(1, 0.2);						// toolbar
+        this.cardBox = this.add.rectangle(this.x, this.y*1.76, this.width, this.height, 0xe76f51).setScale(0.162, 0.204);	// card the player is holding
         this.cardBox.on("pointerover", () => {
             if (this.currentCard == 0) {
 				this.cardBox.setFillStyle(0xb6563e);
@@ -47,7 +48,8 @@ export default class playerView extends Phaser.Scene {
 			}
 		});
         this.currentCardBox = this.add.text(this.x, this.y*1.76, '+', {color: "0x000000"}).setOrigin(0.5).setFontSize(32);	// text displayed on the box for the card the player is holding
-		this.currentCardImage = this.add.image(this.x, this.y*1.76, 2).setScale(0.35).setVisible(false);					//image displayed on the current card box
+		this.currentCardImage = this.add.image(this.x, this.y*1.76, 2).setScale(0.3).setVisible(false);					//image displayed on the current card box
+		
 
 
         // setting up the activity cards
@@ -62,8 +64,10 @@ export default class playerView extends Phaser.Scene {
         this.cards.push([new CardBox(this, 0)]);
         new AddCardBox(this, -1);
         new AddCardBox(this, 1);
-        new CardDiscardBox(this, 0.175, 0.125, 0.15, 0.1); // Button for discarding the currently held card (Only possible to discard cards that are impossible to play)
+        new CardDiscardBox(this, 1.33, 1.875, 0.15, 0.1); // Button for discarding the currently held card (Only possible to discard cards that are impossible to play)
         
+		
+		
 		// a button to pick up a card from the stack
         this.activityCards = [];
         for (let s = 1; s < 5; s++) {
@@ -74,7 +78,12 @@ export default class playerView extends Phaser.Scene {
                 }
             });
         }
-        
+		
+		
+		
+		// button to move to next player/stage
+		new ToolbarButton(this, 0.19, 0.18, "Next Player", goToNextStage, undefined, undefined);
+		
         
         // setting up the board scrolling
         //var cardCamera = this.cameras.add(400, 30, this.width*0.98, this.height*0.75)
